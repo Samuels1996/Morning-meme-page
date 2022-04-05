d12a7789291f0fa569a3ae4edf9304d6af3e34cd
sirmojosodope <stoverlee29@gmail.com>
2022-04-05 14:02:10 -0400

    First Draft for Jung Food

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+const food = [
+    "Rice with spam",
+    "Rice with Kimchi stew",
+    "Rice with mapo tofu",
+    "Rice with seaweed soup",
+    "Sushi",
+    "Rice with ramen",
+    "Omelette Rice",
+    "Protien Bar",
+];
+
+
